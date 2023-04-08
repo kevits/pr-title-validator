05677ef62e0172d924cee358df8ff7347bf5328f
kevits <4051421+kevits@users.noreply.github.com>
2023-04-08 21:12:30 +0000

    parse workflow input

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,55 @@
-import { getInput } from "@actions/core"
+import { getInput, warning } from "@actions/core"
 
 type WorkflowInput = {
     letFail: boolean
-    skipPrefix: string
-    validTypes: string[]
-    validScoped: string[]
-    maxLenght: number
-    regex: RegExp
+    skipPrefix: string | null
+    validTypes: string[] | null
+    validScopes: string[] | null
+    maxLenght: number | null
+    regex: RegExp | null
+}
+
+function parseStringProperty(name: string): string | null {
+    return getInput(name) || null
+}
+
+function parseNumberProperty(name: string): number | null {
+    let inputStr: string | null = getInput(name) || null
+    let inputNum: number | null = Number(inputStr)
+    if (Number.isNaN(inputNum) || inputNum <= 0) {
+        inputNum = null
+    }
+    return inputNum
+}
+
+function parseStringArrayProperty(name: string): string[] | null {
+    let inputStr: string | null = getInput(name) || null
+    if (inputStr != null) {
+        return inputStr.split(",")
+    }
+    return null
+}
+
+function parseRegexProperty(name: string): RegExp | null {
+    try {
+        let inputStr: string | null = getInput(name) || null
+        if (inputStr != null) {
+            return RegExp(inputStr)
+        }
+        return null
+    } catch (e) {
+        warning(`Could not parse regex expression, falling back to default.\n${e}`)
+        return null
+    }
 }
 
 export function getWorkflowInput(): WorkflowInput {
     return {
-        letFail: true,
+        letFail: Boolean(getInput("let-fail")),
+        skipPrefix: parseStringProperty("skip-prefix"),
+        validTypes: parseStringArrayProperty("valid-types"),
+        validScopes: parseStringArrayProperty("valid-scopes"),
+        maxLenght: parseNumberProperty("max-length"),
+        regex: parseRegexProperty("regex")
     }
 }
